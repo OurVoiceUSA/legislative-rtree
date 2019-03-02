faff048bf8760fcbbcfeec40618e8dd3f83a8d6e
Corey Henderson <corman@cormander.com>
2019-03-02 09:34:18 -0700

    split out search to legislative-rtree.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
-import rbush from 'rbush';
+import LRTree from './legislative-rtree';
 import {geolocated} from 'react-geolocated';
-import {ingeojson} from 'ourvoiceusa-sdk-js';
 
 function _browserLocation(props) {
   if (!props.isGeolocationAvailable || !props.isGeolocationEnabled)
@@ -12,39 +11,6 @@ function _browserLocation(props) {
       lat: props.coords.latitude
     };
   return {};
-}
-
-async function rtree2pip(bb, lng, lat) {
-  let uri_base = 'https://raw.githubusercontent.com/OurVoiceUSA/districts/gh-pages/';
-  let file;
-
-  switch (bb.type) {
-  case 'state':
-    file = uri_base+'/states/'+bb.state+'/shape.geojson';
-    break;
-  case 'sldl':
-  case 'sldu':
-    file = uri_base+'/states/'+bb.state+'/'+bb.type+'/'+bb.name+'.geojson';
-    break;
-  case 'cd':
-    file = uri_base+'/cds/2016/'+bb.name+'/shape.geojson';
-    break;
-  default:
-    console.warn("Unknown district type");
-    return false;
-  }
-
-  try {
-    let res = await fetch(file);
-    let geo = await res.json();
-    if (geo.geometry) geo = geo.geometry;
-    if (ingeojson(geo, lng, lat)) {
-      return true;
-    }
-  } catch (e) {
-    console.warn(e);
-  }
-  return false;
 }
 
 class App extends Component {
@@ -62,32 +28,16 @@ class App extends Component {
   }
 
   _loadData = async () => {
-    let res = await fetch('./rtree.json');
-    let rtree = await res.json();
-    this.tree = rbush(9).fromJSON(rtree);
+    let tree = new LRTree();
 
     let location = _browserLocation(this.props);
-    let districts = [];
-    let prom = [];
-    let bbs = [];
 
-    this.tree.search({
-      minX: location.lng,
-      minY: location.lat,
-      maxX: location.lng,
-      maxY: location.lat,
-    }).forEach(bb => {
-      bbs.push(bb);
-      prom.push(rtree2pip(bb, location.lng, location.lat));
-    });
+    if (!location.lng) {
+      setTimeout(() => this._loadData(), 500);
+      return;
+    }
 
-    let ret = await Promise.all(prom);
-
-    bbs.forEach((bb, idx) => {
-      if (ret[idx]) {
-        districts.push("You are located in "+bb.state+" "+bb.type+" "+bb.name);
-      }
-    });
+    let districts = await tree.getDistricts(location.lng, location.lat);
 
     this.setState({districts});
   }
@@ -112,7 +62,7 @@ class App extends Component {
 const District = props => {
   return (
     <div>
-      {props.dist}
+      You are located in {props.dist.state} {props.dist.type} {props.dist.name}
     </div>
   );
 }
